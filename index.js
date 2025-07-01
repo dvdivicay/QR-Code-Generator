53ab7b590247dc3d823acb6c6647463f597a6bc9
dvdivicay <dvdivicay23@gmail.com>
2025-07-01 18:11:45 +0800

    FINAL

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-// var generateName = require('sillyname');
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
 
-import {randomSuperhero} from 'superheroes';
+inquirer
+  .prompt([{
+    message: "Type in your URL:",
+    name: "URL"
+  }])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('URL_img.png'));
 
-var superHeroName = randomSuperhero();
-
-console.log(`My name is ${superHeroName}!`);
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("URL has been saved as a text file!");
+    })
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
